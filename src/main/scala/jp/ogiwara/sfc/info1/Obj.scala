4e5f37924ee1dcbc1af487b0e773b8c53a1a9bf6
ogiwara <yushiogiwara@gmail.com>
2019-05-30 17:50:58 +0900

    add Obj and it's scanner

--- src/main/scala/jp/ogiwara/sfc/info1/Obj.scala
+++ src/main/scala/jp/ogiwara/sfc/info1/Obj.scala
@@ -5,8 +5,39 @@ case class Obj(vertex: Seq[Vector]){
 }
 
 object Obj{
+  def empty(): Obj = new Obj(vertex = Seq())
+
   def fromText(text: String): Obj ={
+
+    var obj = Obj.empty()
     //一行づつ読んでいく
-    Obj(Seq())
+    text.lines.foreach { str =>
+      val element: ObjElement = str.charAt(0) match {
+        case '#' => new CommentObjElement()
+        case 'v' => new VertexObjElement()
+        case  _ => ???
+      }
+
+      obj = element.applyTo(obj)
+    }
+
+    obj
+  }
+}
+
+trait ObjElement{
+  def applyTo(obj: Obj): Obj
+}
+
+sealed class VertexObjElement extends ObjElement{
+  override def applyTo(obj: Obj): Obj = {
+    obj
+  }
+}
+
+sealed class CommentObjElement extends ObjElement{
+  override def applyTo(obj: Obj): Obj = {
+    // 何もしない
+    obj
   }
 }
