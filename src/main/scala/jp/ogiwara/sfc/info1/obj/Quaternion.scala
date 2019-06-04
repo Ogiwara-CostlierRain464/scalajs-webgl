45fa8b5e2ffc1bce25956da58e470226798a0c90
ogiwara <yushiogiwara@gmail.com>
2019-06-04 11:30:20 +0900

    add Quaternion

---
+++ src/main/scala/jp/ogiwara/sfc/info1/obj/Quaternion.scala
@@ -0,0 +1,38 @@
+package jp.ogiwara.sfc.info1.obj
+
+import Math._
+
+// 四元数
+// q = a + bi + cj + dk
+case class Quaternion(a: Float, b: Float, c: Float, d: Float){
+
+  @inline def w: Float = a
+  @inline def x: Float = b
+  @inline def y: Float = c
+  @inline def z: Float = d
+
+  // 共役数
+  def conjugate: Quaternion = Quaternion(a,-b,-c,-d)
+
+  def norm: Float =
+    sqrt(pow(a,2) + pow(b,2) + pow(c,2) + pow(d,2)).toFloat
+
+  def %*% (other: Quaternion): Quaternion ={
+    val (aw, ax, ay, az) = tuple
+    val (bw, bx, by, bz) = other.tuple
+
+    val _x = ax * bw + aw * bx + ay * bz - az * by
+    val _y = ay * bw + aw * by + az * bx - ax * bz
+    val _z = az * bw + aw * bz + ax * by - ay * bx
+    val _w = aw * bw - ax * bx - ay * by - az * bz
+
+    Quaternion(_w, _x, _y, _z)
+  }
+
+  def tuple: (Float, Float, Float, Float) = (a,b,c,d)
+}
+
+
+object Quaternion{
+  def identity: Quaternion = Quaternion(1,0,0,0)
+}
