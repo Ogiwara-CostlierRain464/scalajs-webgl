f8baabd5ac8deffb3e827487144952c35096b0ac
ogiwara <yushiogiwara@gmail.com>
2019-06-11 22:29:14 +0900

    now just state

--- src/main/scala/jp/ogiwara/sfc/info1/physics/NormalPhysicsSystem.scala
+++ src/main/scala/jp/ogiwara/sfc/info1/physics/NormalPhysicsSystem.scala
@@ -15,29 +15,6 @@ class NormalPhysicsSystem extends System{
   var map: mutable.Map[EntityID, RigidBody] = mutable.Map()
 
   override def update(state: WorldState): WorldState = {
-    // 重力を加えるだけなのもよし！
-    val newEntities = state.entities.map { entity =>
-      if(!map.contains(entity.id)){
-        map += (entity.id -> RigidBody(0, Vector3(0,0,0)))
-      }
-
-      val rigidBody = map(entity.id)
-
-      val update = rigidBody.copy(
-        speed = rigidBody.speed + Vector3(0, - (-rigidBody.speed.y + 0.000323), 0)
-      )
-
-      map(entity.id) = update
-
-      entity.position = entity.position.vector + update.speed
-
-      entity
-    }
-
-    val newState = state.copy(
-      entities = newEntities
-    )
-
-    newState
+    state
   }
 }
