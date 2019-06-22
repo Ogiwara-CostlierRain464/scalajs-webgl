809db7852fafad50552603ce33a9e76cec2fddf2
ogiwara <yushiogiwara@gmail.com>
2019-06-22 11:16:25 +0900

    update Shape, before refactoring RigidBody

--- src/main/scala/jp/ogiwara/sfc/info1/physics/elements/Collidable.scala
+++ src/main/scala/jp/ogiwara/sfc/info1/physics/elements/Collidable.scala
@@ -4,6 +4,7 @@ import jp.ogiwara.sfc.info1.math.Vector3
 import jp.ogiwara.sfc.info1.render.Position
 
 case class Collidable(
+                       // 凹状物体を、複数の凸状物体で表現するため
                        shapes: Seq[Shape],
                        // AABBの中心
                        AABBCenter: Position,
--- src/main/scala/jp/ogiwara/sfc/info1/physics/elements/Shape.scala
+++ src/main/scala/jp/ogiwara/sfc/info1/physics/elements/Shape.scala
@@ -1,5 +1,10 @@
 package jp.ogiwara.sfc.info1.physics.elements
 
+import jp.ogiwara.sfc.info1.math.{Quaternion, Vector3}
 import jp.ogiwara.sfc.info1.render.Mesh
 
-case class Shape(mesh: Mesh)
+case class Shape(
+                  mesh: Mesh,
+                  offsetPosition: Vector3,
+                  offsetRotation: Quaternion
+                )
