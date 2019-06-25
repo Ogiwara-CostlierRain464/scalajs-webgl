39c70275052ac8a79c5f9ba5e36412052ddbaf41
ogiwara <yushiogiwara@gmail.com>
2019-06-25 19:41:49 +0900

    remove Polygon class

--- src/main/scala/jp/ogiwara/sfc/info1/render/Polygons.scala
+++
@@ -1,7 +0,0 @@
-package jp.ogiwara.sfc.info1.render
-
-/**
-  * PolygonのCollection Object
-  */
-case class Polygons(items: Seq[Polygon]){
-}
--- src/main/scala/jp/ogiwara/sfc/info1/render/Shader.scala
+++ src/main/scala/jp/ogiwara/sfc/info1/render/Shader.scala
@@ -2,9 +2,7 @@ package jp.ogiwara.sfc.info1.render
 
 import org.scalajs.dom.raw.WebGLShader
 
-case class Shader(rawShader: WebGLShader, aType: ShaderType){
-
-}
+case class Shader(rawShader: WebGLShader, aType: ShaderType)
 
 sealed trait ShaderType
 case object VertexShader extends ShaderType
