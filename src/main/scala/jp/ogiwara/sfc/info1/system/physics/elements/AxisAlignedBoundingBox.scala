44791e6f049d01a09fbdeadb2b5d2a2122c57c66
ogiwara <yushiogiwara@gmail.com>
2019-06-27 14:29:06 +0900

    update AABB

--- src/main/scala/jp/ogiwara/sfc/info1/system/physics/elements/AxisAlignedBoundingBox.scala
+++ src/main/scala/jp/ogiwara/sfc/info1/system/physics/elements/AxisAlignedBoundingBox.scala
@@ -1,8 +1,15 @@
 package jp.ogiwara.sfc.info1.system.physics.elements
 
 import jp.ogiwara.sfc.info1.math.Vector3
+import jp.ogiwara.sfc.info1.system.physics.units.LocalPosition
+import jp.ogiwara.sfc.info1.world.units.Size
 
 /**
   * 軸平行境界ボックス
   */
-case class AxisAlignedBoundingBox()
+case class AxisAlignedBoundingBox(
+                                   // AABBの中心
+                                   center: LocalPosition,
+                                   // AABBのサイズの半分
+                                   half: Size
+                                 )
